aa31aaccf2787edfa9bb9d8aa7fd3538e508ff96
abhi1994j <abhibaban4@gmail.com>
2025-07-08 19:29:39 +0530

    13.js

--- 11.js
+++ 11.js
@@ -6,7 +6,16 @@ console.log(removeDuplicates ([5, 6, 7, 7, 8, 8, 9])); // Output: [5, 6, 7, 8,9]
 console.log(removeDuplicates ([1, 2, 3, 4])); // Output: [1, 2, 3, 4]
 console.log(removeDuplicates ([])); // Output: []
 
+// function removeDuplicates(arr){
+//   if(arr.length === 0) return arr;
+//   return [... new Set(arr)];
+// }
+
 function removeDuplicates(arr){
+  let result = []
   if(arr.length === 0) return arr;
-  return [... new Set(arr)];
+    for(let i= 0 ; i<arr.length;i++){
+      if(!result.includes(arr[i])) result.push(arr[i])
+    }
+   return result
 }
