376f0ea0d5a0a8b0a9127dc2ef86146bccd8197c
abhi1994j <abhibaban4@gmail.com>
2025-07-09 20:31:17 +0530

    14.js

---
+++ 14.js
@@ -0,0 +1,16 @@
+//*-
+//* Interview Question:
+//*: Write a function to calculate the sum of squares of all elements in an array. For example, given the array [1, 2, 3], the function should return 14 because 1*1 + 2*2 + 3*3 = 1 + 4 + 9 = 14.
+
+// Example usage:
+console.log(sumOfSquares([1, 2, 3])); // Output: 14
+console.log(sumOfSquares([1, 2, 7])); // Output: 54
+console.log(sumOfSquares([1, 2, 9])); // Output: 86
+
+function sumOfSquares(arr){
+  let count = 0;
+  for(let ele of arr) {
+    count += ele*ele
+  }
+  return count;
+}
