9d5a4d78813d34021552196ba1af4cd992e4e5e0
abhi1994j <abhibaban4@gmail.com>
2025-07-10 19:12:29 +0530

    15.js

--- 16.js
+++ 16.js
@@ -6,7 +6,7 @@
 console.log(toCamelCase("hello world abhishek")); // Output: helloWorld
 
 //todo HomeWork:
-console.log(toSnakeCase("hello world")); // Output: hello_world
+console.log(toSnakeCase("helloWorld")); // Output: hello_world
 
 function toCamelCase(str){
   console.log(str.split(" "));
@@ -19,6 +19,10 @@ function toCamelCase(str){
   return camalCase;
 }
 
+// function toSnakeCase(str){
+//   return str.replace(/ /g , '_');
+// }
+
 function toSnakeCase(str){
-  return str.replace(/ /g , '_');
+  return str.replace(/([a-z])([A-Z])/g , '$1_$2').toLowerCase()
 }
